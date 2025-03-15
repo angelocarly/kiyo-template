7c5798360d4aea69d517dd6f9db9e8ccf428fb8d
Angelo Carly <angelo.carly@protonmail.com>
2025-03-15 09:54:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kiyo template
+
+This repository acts as a template to easily get started using kiyo.
+
+It is a Rust project setup with kiyo as a dependency, and contains the necessary code to run a simple shader.
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use kiyo::app::app::{App, AppConfig};
+use kiyo::app::draw_orch::{ClearConfig, DispatchConfig, DrawConfig, ImageConfig, Pass};
+use kiyo::app::audio_orch::{AudioConfig};
+
+fn main() {
+
+    let app_config = AppConfig {
+        width: 1000,
+        height: 1000,
+        vsync: true,
+        log_fps: false,
+        fullscreen: false,
+    };
+
+    let config = DrawConfig {
+        images: Vec::from([
+            ImageConfig {
+                clear: ClearConfig::Color(0.0, 0.0, 0.0)
+            },
+        ]),
+        passes: Vec::from([
+            Pass {
+                shader: "shaders/colors.comp".to_string(),
+                dispatches: DispatchConfig::FullScreen,
+                input_resources: Vec::from([]),
+                output_resources: Vec::from([ 0 ]),
+            },
+        ])
+    };
+
+    App::run(app_config, config, AudioConfig::None);
+}
